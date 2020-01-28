732072ed63210cd8242a7b914399ca9dbe723d07
brunomartinsalexandre1995 <bruno.martins.alexandre.1995@gmail.com>
2020-01-28 21:42:20 +0000

    First Commit

---
+++ src/main/java/spring/certificate/training/certificatetopics/CertificateTopicsApplication.java
@@ -0,0 +1,13 @@
+package spring.certificate.training.certificatetopics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateTopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificateTopicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/certificate/training/certificatetopics/CertificateTopicsApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.certificate.training.certificatetopics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateTopicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
